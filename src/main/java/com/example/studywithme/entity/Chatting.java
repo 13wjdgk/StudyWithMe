1e0c4396eed964bd9d57f540e04dd3a688016934
Minsu17 <tjalstn1119@naver.com>
2024-09-11 10:14:55 +0900

    test chatting

--- src/main/java/com/example/studywithme/entity/Chatting.java
+++ src/main/java/com/example/studywithme/entity/Chatting.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+// test
 @Entity
 @Table(name = "Chatting")
 public class Chatting {
