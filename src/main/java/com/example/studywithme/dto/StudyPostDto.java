5ca68e2be7aa790cb51ff0f37e78e46f9640a4a5
13wjdgk <13wjdgk@naver.com>
2024-09-13 17:52:44 +0900

    merge : StudyPostDto 수정

--- src/main/java/com/example/studywithme/dto/StudyPostDto.java
+++ src/main/java/com/example/studywithme/dto/StudyPostDto.java
@@ -1,60 +1,63 @@
 package com.example.studywithme.dto;
 
-import com.example.studywithme.enums.MeetType;
-import lombok.Data;
-
 import java.sql.Timestamp;
 import java.time.LocalDate;
 
-@Data
-public class StudyPostDto {
+import com.example.studywithme.enums.MeetType;
 
-    private Integer postId;
-    private String title;
-    private String description;
-    private String studyType;
-    private LocalDate studyDate;
-    private LocalDate endDate;
-    private LocalDate deadline;
-    private Integer maxMembers;
-    private Timestamp createdAt;
-    private String userId;
-    private int categoryId;
+import lombok.Getter;
+import lombok.Setter;
 
-    private Boolean language;
-    private Boolean certification;
-    private Boolean major;
-    private Boolean career;
-    private Boolean exam;
-    private Boolean hobbies;
-    private Boolean programming;
-    private Boolean selfDirected;
-    private Boolean etc;
-    private MeetType meetType;
+@Getter
+@Setter
+public class StudyPostDTO {
 
-    public StudyPostDto(Integer postId, String title, String description, String studyType, LocalDate studyDate,
-                        LocalDate endDate, LocalDate deadline, Integer maxMembers, Timestamp createdAt, int categoryId,
-                        Boolean language, Boolean certification, Boolean major, Boolean career, Boolean exam,
-                        Boolean hobbies, Boolean programming, Boolean selfDirected, Boolean etc, String meetType) {
-        this.postId = postId;
-        this.title = title;
-        this.description = description;
-        this.studyType = studyType;
-        this.studyDate = studyDate;
-        this.endDate = endDate;
-        this.deadline = deadline;
-        this.maxMembers = maxMembers;
-        this.createdAt = createdAt;
-        this.categoryId = categoryId;
-        this.language = language;
-        this.certification = certification;
-        this.major = major;
-        this.career = career;
-        this.exam = exam;
-        this.hobbies = hobbies;
-        this.programming = programming;
-        this.selfDirected = selfDirected;
-        this.etc = etc;
-        this.meetType = MeetType.valueOf(meetType);
-    }
+	private Integer postId;
+	private String title;
+	private String description;
+	private String studyType;
+	private LocalDate studyDate;
+	private LocalDate endDate;
+	private LocalDate deadline;
+	private Integer maxMembers;
+	private Timestamp createdAt;
+	private String categoryId;
+
+	private Boolean language;
+	private Boolean certification;
+	private Boolean major;
+	private Boolean career;
+	private Boolean exam;
+	private Boolean hobbies;
+	private Boolean programming;
+	private Boolean selfDirected;
+	private Boolean etc;
+	private MeetType meetType;
+
+	// Constructor
+	public StudyPostDTO(Integer postId, String title, String description, String studyType, LocalDate studyDate,
+		LocalDate endDate, LocalDate deadline, Integer maxMembers, Timestamp createdAt, String categoryId,
+		Boolean language, Boolean certification, Boolean major, Boolean career, Boolean exam,
+		Boolean hobbies, Boolean programming, Boolean selfDirected, Boolean etc, String meetType) {
+		this.postId = postId;
+		this.title = title;
+		this.description = description;
+		this.studyType = studyType;
+		this.studyDate = studyDate;
+		this.endDate = endDate;
+		this.deadline = deadline;
+		this.maxMembers = maxMembers;
+		this.createdAt = createdAt;
+		this.categoryId = categoryId;
+		this.language = language;
+		this.certification = certification;
+		this.major = major;
+		this.career = career;
+		this.exam = exam;
+		this.hobbies = hobbies;
+		this.programming = programming;
+		this.selfDirected = selfDirected;
+		this.etc = etc;
+		this.meetType = MeetType.valueOf(meetType);
+	}
 }
