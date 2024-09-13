08a60803fb33122f6680098616816eaeefa86d0c
lixxce5017 <lixxce5017@gmail.com>
2024-09-13 17:03:38 +0900

    채팅상세 부분 불러오기 작업

--- src/main/java/com/example/studywithme/repository/ChattingRepository.java
+++ src/main/java/com/example/studywithme/repository/ChattingRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.studywithme.entity.Chatting;
 
+import java.util.List;
+
 public interface ChattingRepository extends JpaRepository<Chatting, Integer> {
+    List<Chatting> findByChatRoom_ChatroomId(Integer chatRoomId);
 }
--- src/main/java/com/example/studywithme/service/ChatService.java
+++ src/main/java/com/example/studywithme/service/ChatService.java
@@ -2,6 +2,7 @@ package com.example.studywithme.service;
 
 
 import com.example.studywithme.dto.ChatDto;
+import com.example.studywithme.dto.ChatLogResult;
 import com.example.studywithme.dto.ChatRoomDto;
 import com.example.studywithme.dto.ChatRoomResult;
 import com.example.studywithme.entity.*;
@@ -47,5 +48,24 @@ public class ChatService {
         chattingRepository.save(chat);
         System.out.println(chat);
         return;
+    }
+    public ChatLogResult ChattingLog(Integer roomId)
+    {
+        ChatLogResult chatLogResult = new ChatLogResult();
+        List<Chatting> chat =chattingRepository.findByChatRoom_ChatroomId(roomId);
+        List<ChatDto>chatDtos= new ArrayList<>();
+        chat.forEach(chatting -> {
+            ChatDto chatDto = new ChatDto();
+            chatDto.setSendtime(chatting.getSendTime());
+            chatDto.setMessage(chatting.getMessage());
+            chatDto.setSender(chatting.getUser().getNickname());
+            chatDto.setSenderId(chatting.getUser().getUserId());
+            chatDto.setRoomId(chatting.getChatRoom().getChatroomId());
+            chatDtos.add(chatDto);
+        });
+        chatLogResult.setResult("success");
+        chatLogResult.setChatDtoList(chatDtos);
+        System.out.println(chatDtos);
+        return chatLogResult;
     }
 }
